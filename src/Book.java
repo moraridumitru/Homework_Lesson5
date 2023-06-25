67031a95583c1b0c7b18dd8108d0072f82746806
moraridumitru <moraridumitru1@gmail.com>
2023-06-25 11:01:39 +0300

    Java Collections

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,121 @@
-package PACKAGE_NAME;public class Book {
+import java.util.ArrayList;
+import java.util.List;
+
+class Book {
+    private int id;
+    private String title;
+    private String author;
+    private String edition;
+    private int year;
+    private int pageCount;
+    private double price;
+
+    public Book(int id, String title, String author, String edition, int year, int pageCount, double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.edition = edition;
+        this.year = year;
+        this.pageCount = pageCount;
+        this.price = price;
+    }
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public void setEdition(String edition) {
+        this.edition = edition;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public void setPageCount(int pageCount) {
+        this.pageCount = pageCount;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
+
+class BookCollection {
+    private List<Book> books;
+
+    public BookCollection() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+    }
+
+    public List<Book> getBooksByAuthor(String author) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getAuthor().equals(author)) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+
+    public List<Book> getBooksByEdition(String edition) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getEdition().equals(edition)) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
+    public List<Book> getBooksPublishedAfterYear(int year) {
+        List<Book> result = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getYear() > year) {
+                result.add(book);
+            }
+        }
+        return result;
+    }
 }
--- src/BookCollectionDemo.java
+++ src/BookCollectionDemo.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class BookCollectionDemo {
+import java.util.ArrayList;
+import java.util.List;
+public class BookCollectionDemo {
+    public static void main(String[] args) {
+        // Create a new book collection
+        BookCollection bookCollection = new BookCollection();
+
+        // Add books to the collection
+        bookCollection.addBook(new Book(1, "Book 1", "Author 1", "Edition 1", 2000, 320, 12.99));
+        bookCollection.addBook(new Book(2, "Book 2", "Author 2", "Edition 2", 2005, 251, 15.99));
+        bookCollection.addBook(new Book(3, "Book 3", "Author 1", "Edition 1", 2010, 479, 18.99));
+        bookCollection.addBook(new Book(4, "Book 4", "Author 2", "Edition 3", 2011, 500, 19.99));
+        // Get the list of books by a given author
+        List<Book> booksByAuthor = bookCollection.getBooksByAuthor("Author 1");
+        System.out.println("List of books by given author:");
+        for (Book book : booksByAuthor) {
+            System.out.println(book.getTitle());
+        }
+
+        // Get the list of books published by a given edition
+        List<Book> booksByEdition = bookCollection.getBooksByEdition("Edition 1");
+        System.out.println("List of books published by given edition:");
+        for (Book book : booksByEdition) {
+            System.out.println(book.getTitle());
+        }
+
+        // Get the list of books published after a given year
+        List<Book> booksPublishedAfterYear = bookCollection.getBooksPublishedAfterYear(2005);
+        System.out.println("List of books published after given year:");
+        for (Book book : booksPublishedAfterYear) {
+            System.out.println(book.getTitle());
+        }
+    }
 }
