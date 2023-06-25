95159dddb5383d4dac22caa49f4b30321ce627bc
moraridumitru <moraridumitru1@gmail.com>
2023-06-25 11:10:49 +0300

    Java Collections

--- src/BookCollectionDemo.java
+++ src/BookCollectionDemo.java
@@ -10,23 +10,25 @@ public class BookCollectionDemo {
         bookCollection.addBook(new Book(2, "Book 2", "Author 2", "Edition 2", 2005, 251, 15.99));
         bookCollection.addBook(new Book(3, "Book 3", "Author 1", "Edition 1", 2010, 479, 18.99));
         bookCollection.addBook(new Book(4, "Book 4", "Author 2", "Edition 3", 2011, 500, 19.99));
+        bookCollection.addBook(new Book(5, "Book 5", "Author 3", "Edition 1", 2012, 511, 31.99));
+        bookCollection.addBook(new Book(6, "Book 6", "Author 1", "Edition 1", 2015, 210, 11.99));
         // Get the list of books by a given author
         List<Book> booksByAuthor = bookCollection.getBooksByAuthor("Author 1");
-        System.out.println("List of books by given author:");
+        System.out.println("Список книг автора 'Author 1':");
         for (Book book : booksByAuthor) {
             System.out.println(book.getTitle());
         }
 
         // Get the list of books published by a given edition
         List<Book> booksByEdition = bookCollection.getBooksByEdition("Edition 1");
-        System.out.println("List of books published by given edition:");
+        System.out.println("Список книг выпущенных издательством 'Edition 1':");
         for (Book book : booksByEdition) {
             System.out.println(book.getTitle());
         }
 
         // Get the list of books published after a given year
         List<Book> booksPublishedAfterYear = bookCollection.getBooksPublishedAfterYear(2005);
-        System.out.println("List of books published after given year:");
+        System.out.println("Списoк книг выпущенных после '2005' года:");
         for (Book book : booksPublishedAfterYear) {
             System.out.println(book.getTitle());
         }
